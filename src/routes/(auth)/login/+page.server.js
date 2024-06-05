bde9d6cf063bfbc73415755ca62ec1c1108664b8
alannuarii <alannuariii@gmail.com>
2024-06-05 22:04:30 +0800

    Update

--- src/routes/(auth)/login/+page.server.js
+++ src/routes/(auth)/login/+page.server.js
@@ -1,62 +1,36 @@
 import { API_AUTH } from '$env/static/private';
 import { redirect } from '@sveltejs/kit';
 
-
 export const actions = {
-	default: async ({ request, cookies }) => {
-		const data = await request.formData();
+    default: async ({ request, cookies }) => {
+        const data = await request.formData();
 
-		const formData = new FormData();
-		formData.set('email', data.get('email'));
-		formData.set('password', data.get('password'));
+        const formData = new FormData();
+        formData.set('email', data.get('email'));
+        formData.set('password', data.get('password'));
 
         const res = await fetch(`${API_AUTH}/api/login`, {
             method: 'POST',
             body: formData
         });
 
-		const result = await res.json();
+        if (!res.ok) {
+            return { error: 'Login gagal. Silakan coba lagi.' };
+        }
 
-		if (!!result.access_token) {
-			cookies.set('accessToken', result.access_token, { path: '/', sameSite: 'strict' });
-			throw redirect(302, '/');
-		}
+        const result = await res.json();
 
-		return result;
-	}
+        if (result.access_token) {
+            cookies.set('accessToken', result.access_token, {
+                path: '/',
+                sameSite: 'strict',
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production', // Hanya kirimkan cookie pada koneksi HTTPS di production
+                maxAge: 60 * 60 * 24 // 1 hari
+            });
+            throw redirect(302, '/');
+        }
+
+        return result;
+    }
 };
-
-
-// Versi Backend Javascript
-// export const actions = {
-// 	default: async ({ request, cookies }) => {
-// 		const data = await request.formData();
-
-// 		const formData = new FormData();
-// 		formData.set('email', data.get('email'));
-// 		formData.set('password', data.get('password'));
-
-// 		const parsedData = {};
-// 		for await (const [name, value] of formData.entries()) {
-// 			parsedData[name] = value;
-// 		}
-
-// 		const res = await fetch(`${API_AUTH}/api/login`, {
-// 			method: 'POST',
-// 			body: JSON.stringify(parsedData),
-// 			headers: {
-// 				'Content-Type': 'application/json'
-// 			}
-// 		});
-
-// 		const result = await res.json();
-
-
-// 		if (!!result.access_token) {
-// 			cookies.set('accessToken', result.access_token, { path: '/', sameSite: 'strict' });
-// 			throw redirect(302, '/');
-// 		}
-
-// 		return result;
-// 	}
-// };
